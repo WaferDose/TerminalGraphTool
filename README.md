623f782fd6e00e9176df9a68333ec1aed8ac25fd
WaferDose <waferdose@gmail.com>
2026-04-19 21:53:48 +0300

    improved main function and fixed bugs

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
-# This project is **not** maintained in any way.
+# This project is ***not*** maintained in any way.
 
-This is just a one-time project kind of thing.
+This is just a one-time project, kind of thing.
 
 # Description
 
-Just a tool to make graphs on the terminal. (not very functional yet)
+Just a little terminal graphing tool that doesn't really do that much *(Only prints ascii)*.
+
+Can print functions like : `C1 * x^{0,1,2} * y^{0,1,2} + C2 * x^{0,1,2} * y^{0,1,2} + ...` = 0 (for now)
--- graph.cpp
+++ graph.cpp
@@ -38,44 +38,27 @@ class graph
 {
 private:
     bool multiVarFlag;
-    float a, b, c;
-    char *signValues;
+    char drawChar;
     int width, height;
-    int termCount = 0;
+    int termCount;
     functionTerm *term;
-    //void OLDgenerateSignValues();
-    double calculateValue(int x,int y);
+    double calculateValue(float x,float y);
 public:
-    //void OLDsetValues(float C1, float C2, float C3);
+    int getWidth();
+    int getHeight();
     void input(string);
     void printGraph();
+    void changeWidth(int);
+    void changeHeight(int);
+    void changeDrawChar(char);
     void DEBUG_print_signValues();
-    //void OLDprintGraph();
-    //void OLDDEBUG_print_signValues();
     void DEBUG_print_terms();
     graph(int x, int y);
     ~graph();
 };
 
-// void graph::OLDgenerateSignValues()
-// {
-//     for(int y=height;y>0;y--)
-//     {
-//         for(int x=1;x<width;x++)
-//         {
-//             if(((a*x*x+b*x+c)-y)<0)
-//             {
-//                 signValues[(y-1)*width+(x-1)] = 1;
-//             }
-//             else
-//             {
-//                 signValues[(y-1)*width+(x-1)] = -1;
-//             }
-//         }
-//     }
-// }
 
-double graph::calculateValue(int x,int y)
+double graph::calculateValue(float x,float y)
 {
     double value = 0,temp;
     for(int i=0;i<termCount;i++)
@@ -90,32 +73,36 @@ double graph::calculateValue(int x,int y)
     return value;
 }
 
-// void graph::OLDsetValues(float C1, float C2, float C3)
-// {
-//     a = C1;
-//     b = C2;
-//     c = C3;
-// }
+int graph::getWidth()
+{
+    return width;
+}
 
-void graph::input(string str)
+int graph::getHeight()
+{
+    return height;
+}
+
+void graph::input(string str)           // tbh (string str) here is useless (as input)
 {
     bool badStringFlag, constFlag, varFlag, expectNumberFlag;              // constflag, varflag indeicate whether const/var stage is done (complete)
     termCount = 0;
     multiVarFlag = 0;
 
             // 3XY+4-XXYY // 5Y + 6XX // 0.1y // 0X //     // +3 // 3- // 3 // 3X3 //
-    cout << "Type : C1{x:0,1,2}{y:0,1,2}+C2{x:0,1,2}{y:0,1,2}+...+Cn{x:0,1,2}{y:0,1,2} (=0)" << endl << "e.g. 31XXY+2Y+5 (=0)" << endl;
+    cout << "Type : C1{x:0,1,2}{y:0,1,2}+C2{x:0,1,2}{y:0,1,2}+...+Cn{x:0,1,2}{y:0,1,2} (=0)" << endl << "e.g. 31XXY+2Y+5 (=0)   (or 3X (=Y) if no Y as input)" << endl;
     do
     {
         if(badStringFlag)
-            cout << "Please try again" << endl;
+            cout << "Please try again." << endl;
         badStringFlag = 0, expectNumberFlag = 0;
         cin >> str;
+        cin.get();
         for(int i=0;i<str.length();i++)
         {
             if(!((str[i] >= '0' && str[i] <= '9') || str[i] == '.'|| toupper(str[i]) == 'X' || toupper(str[i]) == 'Y' || str[i] == '+' || str[i] == '-'))
             {
-                badStringFlag = 1; cout << "DEBUG 1" << endl;
+                badStringFlag = 1;
                 break;
             }
         }
@@ -124,16 +111,21 @@ void graph::input(string str)
 
         if(str[0] == '+')
         {
-            badStringFlag = 1; cout << "DEBUG 2" << endl;
+            badStringFlag = 1;
             continue;
         }
         
-
+        for(int i=0;i<str.length();i++)
+        {
+            if(toupper(str[i]) == 'Y')
+            {
+                multiVarFlag = 1;
+                cout << "MULTI" << endl;
+            }
+        }
         termCount = 1;
         for(int i=1;i<str.length();i++)
         {
-            if(toupper(str[i]) == 'Y')
-                multiVarFlag = 1;
             if(str[i] == '+' || str[i] == '-')
                 termCount++;
         }
@@ -164,16 +156,16 @@ void graph::input(string str)
         }
         if(!constFlag && !varFlag)
         {
-            badStringFlag = 1; cout << "DEBUG 3" << endl;
+            badStringFlag = 1;
             continue;
         }
         if((termCount == 1) && j != str.length())
         {
-            badStringFlag = 1;  cout << "DEBUG 10" << endl;
+            badStringFlag = 1;
         }
         if((termCount == 1) && !varFlag)
         {
-            badStringFlag = 1;  cout << "DEBUG 11" << endl;
+            badStringFlag = 1;
         }
 
         // Big loop
@@ -186,7 +178,7 @@ void graph::input(string str)
             }
             else
             {
-                badStringFlag = 1; cout << "DEBUG 8" << endl;
+                badStringFlag = 1;
             }
             while((str[j] >= '0' && str[j] <= '9') || str[j] == '.')
             {
@@ -200,7 +192,7 @@ void graph::input(string str)
             }
             if(!constFlag && !varFlag)
             {
-                badStringFlag = 1; cout << "DEBUG 4" << endl;
+                badStringFlag = 1;
                 break;
             }
         }
@@ -216,7 +208,7 @@ void graph::input(string str)
             }
             else
             {
-                badStringFlag = 1; cout << "DEBUG 9" << endl;
+                badStringFlag = 1;
             }
             constFlag = 0, varFlag = 0;
             while((str[j] >= '0' && str[j] <= '9') || str[j] == '.')
@@ -232,7 +224,7 @@ void graph::input(string str)
             }
             if(!constFlag && !varFlag)
             {
-                badStringFlag = 1; cout << "DEBUG 5" << endl;
+                badStringFlag = 1;
                 continue;
             }
         }
@@ -247,13 +239,16 @@ void graph::input(string str)
 
     delete [] term;
     term = new functionTerm[termCount];
-    int ind=0, xcount, ycount;
+    int ind=0, xcount, ycount, offset = 0;
     for(int i=0;i<termCount;i++)
     {
         xcount = 0; ycount = 0;
-        if((atof(str.c_str()+ind) == 0) && str[ind] != '0')
+        if((atof(str.c_str()+ind) == 0) && str[ind+offset] != '0')
         {
-            term[i].setConst(1);
+            if(str[ind] == '-')
+                term[i].setConst(-1);
+            else
+                term[i].setConst(1);
         }
         else term[i].setConst(atof(str.c_str()+ind));
 
@@ -272,21 +267,32 @@ void graph::input(string str)
             ind++;
         }
         term[i].setPower(xcount*3+ycount);          // Power = x*3 + y
+        offset = 1;
     }
 }
 
 void graph::printGraph()
 {
+    float corrector;
     for(int y=height-1;y>0;y--)
     {
-        cout << "|";
-        for(int x=1;x<width-1;x++)
+        corrector = 0;
+        if(!(y%10))
+        {
+            cout << y;
+            corrector = 0.5;
+        }
+        else
+        {
+            cout << "|";
+        }
+        for(float x=1+corrector;x<width-1;x+=0.5)
         {
             if((calculateValue(x-1,y-1) > 0) && (calculateValue(x-1,y) > 0) && (calculateValue(x,y-1) > 0) && (calculateValue(x,y) > 0) || (calculateValue(x-1,y-1) < 0) && (calculateValue(x-1,y) < 0) && (calculateValue(x,y-1) < 0) && (calculateValue(x,y) < 0))
             {
-                cout << "  ";
+                cout << " ";
             }
-            else cout << "##";
+            else cout << drawChar;
         }
         cout << endl;
     }
@@ -297,37 +303,28 @@ void graph::printGraph()
     cout << endl;
 }
 
-// void graph::OLDprintGraph()
-// {
-//     OLDgenerateSignValues();
-//     for(int y=height-1;y>0;y--)
-//     {
-//         cout << "|";
-//         for(int x=1;x<width-1;x++)
-//         {
-//             if((signValues[(y-1)*width+(x-1)] == 1) && (signValues[(y-1)*width+(x)] == 1) && (signValues[(y)*width+(x-1)] == 1) && (signValues[(y)*width+(x)] == 1) || (signValues[(y-1)*width+(x-1)] == -1) && (signValues[(y-1)*width+(x)] == -1) && (signValues[(y)*width+(x-1)] == -1) && (signValues[(y)*width+(x)] == -1))
-//             {
-//                 cout << "  ";
-//             }
-//             else cout << "##";
-//         }
-//         cout << endl;
-//     }
-//     for(int x=0;x<width;x++)
-//     {
-//         cout << "==";
-//     }
-//     cout << endl;
-// }
-
-graph::graph(int x=104, int y=50)
+void graph::changeWidth(int temp)
 {
-    width = x;
-    height = y;
+    width = temp;
+}
+
+void graph::changeHeight(int temp)
+{
+    height = temp;
+}
+
+void graph::changeDrawChar(char temp)
+{
+    drawChar = temp;
+}
+
+graph::graph(int x=104, int y=51)
+{
+    drawChar = '#';
+    termCount = 0;
+    changeWidth(x);
+    changeHeight(y);
     term = new functionTerm[0];
-    signValues = new char [(x-1)*(y-1)];
-    a = b = 0;
-    c = 5;
 }
 
 graph::~graph()
@@ -335,45 +332,21 @@ graph::~graph()
     delete [] term;
 }
 
-// void graph::OLDDEBUG_print_signValues()
-// {
-//     for(int y=height;y>0;y--)
-//     {
-//         cout << "|";
-//         for(int x=1;x<width;x++)
-//         {
-//             if(((a*x*x+b*x+c)-y)<0)
-//             {
-//                 cout << "++";
-//             }
-//             else
-//             {
-//                 cout << "--";
-//             }
-//         }
-//         cout << endl;
-//     }
-//     for(int x=0;x<width;x++)
-//     {
-//         cout << "==";
-//     }
-//     cout << endl;
-// }
 
 void graph::DEBUG_print_signValues()
 {
     for(int y=height;y>0;y--)
     {
         cout << "|";
-        for(int x=1;x<width;x++)
+        for(float x=1;x<width;x+=0.5)
         {
             if(calculateValue(x,y)<0)
             {
-                cout << "--";
+                cout << "-";
             }
             else
             {
-                cout << "++";
+                cout << "+";
             }
         }
         cout << endl;
@@ -399,21 +372,157 @@ void graph::DEBUG_print_terms()
     }
 }
 
-int main()
+int main()                                                  // MAIN
 {
     graph mainGraph;
     string str_input;
-    //cout << "Type : C1{x:0,1,2}{y:0,1,2}+C2{x:0,1,2}{y:0,1,2}+...+Cn{x:0,1,2}{y:0,1,2} (=0)" << endl << "e.g. 31XXY+2Y+5 (=0)" << endl;
-    //cout << "Type C1,C2,C3 where: C1x^2 + C2x + C1 (=0)" << endl << "e.g. 31, 2, 5  (C1, C2, C3)" << endl;
-    //cin >> input;
-    mainGraph.input(str_input);
-    mainGraph.DEBUG_print_terms();
-    mainGraph.DEBUG_print_signValues();
-    mainGraph.printGraph();
-    //mainGraph.OLDsetValues(1,1,0);
-    //mainGraph.OLDDEBUG_print_signValues();
-    //mainGraph.OLDprintGraph();
-    cin >> str_input;
+    bool mainLoopflag=1, debugPrintTermsFlag=0, debugPrintSVFlag=0;
+    int int_input;
+    do
+    {
+        cout << "Select action : \n";
+        cout << " 1. Print\n";
+        cout << " 2. Change graph options\n";
+        cout << " 3. Debug options\n";
+        cout << " 4. Exit" << endl;
+        cin >> str_input;
+        cin.get();
+        int_input = atoi(str_input.c_str());
+        switch(int_input)
+        {
+            case 1 :
+            {
+                mainGraph.input(str_input);
+                if(debugPrintTermsFlag)
+                    mainGraph.DEBUG_print_terms();
+                if(debugPrintSVFlag)
+                    mainGraph.DEBUG_print_signValues();
+                mainGraph.printGraph();
+                cout << "Press enter to continue" << endl;
+                while(cin.get() != '\n');
+                break;
+            }
+            case 2 :
+            {
+                bool loopflag = 1;
+                do
+                {
+                    cout << " Change graph options : \n";
+                    cout << "  1. Change height\n";
+                    cout << "  2. Change width\n";
+                    cout << "  3. Change draw character\n";
+                    cout << "  4. Back" << endl;
+                    cin >> str_input;
+                    cin.get();
+                    int_input = atoi(str_input.c_str());
+                    switch(int_input)
+                    {
+                        case 1 :
+                        {
+                            cout << "(Current height : " << mainGraph.getHeight() << ")\n";
+                            cout << "Change height to : ";
+                            cin >> str_input;
+                            cin.get();
+                            while(!atoi(str_input.c_str()) && str_input[0])
+                            {
+                                cout << "Invalid input.\nPress enter to continue." << endl;
+                                while(cin.get() != '\n');
+                                cout << "(Current height : " << mainGraph.getHeight() << ")\n";
+                                cout << "Change height to : ";
+                                cin >> str_input;
+                                cin.get();
+                            }
+                            mainGraph.changeHeight(atoi(str_input.c_str()));
+                            cout << "Succesfully changed height to : " << atoi(str_input.c_str()) << "\n" << "\nPress enter to continue" << endl;
+                            while(cin.get() != '\n');
+                            break;
+                        }
+                        case 2 :
+                        {
+                            cout << "(Current width : " << mainGraph.getWidth() << ")\n";
+                            cout << "Change width to : ";
+                            cin >> str_input;
+                            cin.get();
+                            while(!atoi(str_input.c_str()) && str_input[0])
+                            {
+                                cout << "Invalid input.\nPress enter to continue." << endl;
+                                while(cin.get() != '\n');
+                                cout << "(Current width : " << mainGraph.getWidth() << ")\n";
+                                cout << "Change width to : ";
+                                cin >> str_input;
+                                cin.get();
+                            }
+                            mainGraph.changeWidth(atoi(str_input.c_str()));
+                            cout << "Succesfully changed width to : " << atoi(str_input.c_str()) << "\n" << "\nPress enter to continue" << endl;
+                            while(cin.get() != '\n');
+                            break;
+                        }
+                        case 3 :
+                        {
+                            char temp;
+                            cout << "Change draw character to : ";
+                            cin.get(temp);
+                            while(cin.get() != '\n');
+                            while(!isprint(temp))
+                            {
+                                cout << "Non-printable character.\nPress enter to continue." << endl;
+                                while(cin.get() != '\n');
+                                cout << "Change draw character to : ";
+                                cin.get(temp);
+                                while(cin.get() != '\n');
+                            }
+                            mainGraph.changeDrawChar(temp);
+                            cout << "Succesfully changed draw character to : '" << temp << "'\n" << "\nPress enter to continue" << endl;
+                            while(cin.get() != '\n');
+                            break;
+                        }
+                        case 4 : loopflag = 0; break;
+                        default : cout << "Invalid input.\nPress enter to continue." << endl; while(cin.get() != '\n'); break;
+                    }
+                }
+                while(loopflag);
+                
+                break;
+            }
+            case 3 : 
+            {
+                bool loopflag = 1;
+                do
+                {
+                    cout << " Debug options : \n";
+                    cout << "  1. Print function terms = \"" << debugPrintTermsFlag << "\"\n";
+                    cout << "  2. Print sign values = \"" << debugPrintSVFlag << "\"\n";
+                    cout << "  3. Back" << endl;
+                    cin >> str_input;
+                    cin.get();
+                    int_input = atoi(str_input.c_str());
+                    switch(int_input)
+                    {
+                        case 1 : debugPrintTermsFlag ? (debugPrintTermsFlag=0) : (debugPrintTermsFlag=1); break;
+                        case 2 : debugPrintSVFlag ? (debugPrintSVFlag=0) : (debugPrintSVFlag=1); break;
+                        case 3 : loopflag = 0; break;
+                        default : cout << "Invalid input.\nPress enter to continue." << endl; while(cin.get() != '\n'); break;
+                    }
+                }
+                while(loopflag);
+                
+                break;
+            }
+            case 4 : 
+            {
+                mainLoopflag = 0;
+                break;
+            }
+            default : 
+            {
+                cout << "Invalid input.\nPress enter to continue." << endl;
+                while(cin.get() != '\n');
+                break;
+            }
+        }
+    }
+    while(mainLoopflag);
+
     return 0;
 }
 
@@ -423,6 +532,12 @@ int main()
 //     Make input less hardcoded (e.g.3XYX4 could be acceptable)
 //     NO
 
-//     xx should automatically be 1xx not 0xx
+//     xx should automatically be 1xx not 0xx    // GOOD
 
-//
+//     Make the grapher better by not hard-printing '##' and print '#' instead    // GOOD i guess
+
+//     BUG: input sets const to 1 (if no const) regarless of - or + in front of it. (x to 1x AND -x to 1x)    // FIXED
+
+//     Make exponents not max out at 2
+
+//     Make (0,0) movable and not hard-locked in bottom left
